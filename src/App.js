1513cb73a5f019303e47c5282e22c38784345e6b
Plut0r <ogunolaololade@gmail.com>
2022-07-21 13:46:38 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,204 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <h1>Besnik.</h1>
+        <nav className='nav'>
+          <ul className='list-1'>
+            <li><a href="#">Solution</a></li>
+            <li><a href="#">Plans</a></li>
+            <li><a href="#">Resource</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+          <ul className='ul'>
+            <li className='ab1'><a href="#">Request Demo</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div className='section-1'>
+          <div className='section-1-a'>
+            <h2>Tell a better brand story</h2>
+            <p>Automate the way you search through hashtags and suggested
+              profiles to find results 100x faster.
+            </p>
+            <button type="button">Request Demo <span>
+              <img src='/images/Vector.png' alt="icon" />
+            </span>
+            </button>
+          </div>
+          <div className='section-1-b'>
+            <img src='/images/Group 1000001628.png' alt='icon' />
+          </div>
+        </div>
+        <div className='section-2'>
+          <h3>Some of our trusted clients</h3>
+          <div className='section-2-a'>
+            <img src='/images/logo/chase.png' alt='logo' />
+            <img src='/images/logo/asana.png' alt='logo' />
+            <img src='/images/logo/Group 9.png' alt='logo' />
+            <img src='/images/logo/buzzfeed.png' alt='logo' />
+            <img src='/images/logo/Mask Group.png' alt='logo' />
+            <img src='/images/logo/Walmart.png' alt='logo' />
+          </div>
+        </div>
+        <div className='section-3'>
+          <h3>Solutions for every need</h3>
+          <p>Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce.</p>
+        </div>
+        <div className='section-3-a'>
+          <div className='influencer-text'>
+            <h4>Influencer</h4>
+            <h3>Influencer marketing</h3>
+            <p>Create infinite pieces of content with our influencer product. Build,streamline,manage large-scale influencer programs</p>
+            <a>Learn more <span><img src='/images/vector-1.png' /></span></a>
+          </div>
+          <div className='influencer-img'>
+            <img src='/images/Group 43.png' alt='influencer-img' />
+          </div>
+        </div>
+        <div className='section-3-a community'>
+          <div className='community-img'>
+            <img src='/images/profile.png' alt='community-img' />
+          </div>
+          <div className='community-text'>
+            <h4>Community</h4>
+            <h3>Community marketing</h3>
+            <p>Bring your relationships to new heights with our community product. Discover your biggest enthusiasts</p>
+            <a>Learn more <span><img src='/images/vector-1.png' /></span></a>
+          </div>
+        </div>
+        <div className='section-3-a c'>
+          <div className='services-text'>
+            <h4>Services</h4>
+            <h3>Influencer marketing</h3>
+            <p>Create infinite pieces of content with our influencer product. Build,streamline,manage large-scale influencer programs</p>
+            <a>Learn more <span><img src='/images/vector-1.png' /></span></a>
+          </div>
+          <div className='influencer-img'>
+            <img src='/images/card.png' alt='influencer-img' />
+          </div>
+        </div>
+        <div className='search-div'>
+          <h3>Search through more than 11 million social media profiles</h3>
+          <p className='p'>Heepsy’s search filters help you find exactly what you’re looking for, and our influencer reports provide you with the metrics</p>
+          <div className='metrics-div'>
+            <div className='num-div'>
+              <h4>2260</h4>
+              <p>Satisfied Brands</p>
+            </div>
+            <div className='num-div'>
+              <h4>1324</h4>
+              <p>Satisfied Clients</p>
+            </div>
+            <div className='num-div'>
+              <h4>3453</h4>
+              <p>Sponsored</p>
+            </div>
+            <div className='num-div'>
+              <h4>9786</h4>
+              <p>Completed</p>
+            </div>
+          </div>
+        </div>
+        <div className='section-3 solution-div'>
+          <h3>Solutions for every need</h3>
+          <p>Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce.</p>
+        </div>
+        <div className="main-clothing-div">
+          <div className="clothing-div">
+            <img className="clothing-img" src="/images/image/image01.png" alt="image01" />
+            <div className="card-div">
+              <h5 className="rebel">Rebel clothing</h5>
+              <div className="followers-div">
+                <div className="followers">
+                  <h5>6.7M</h5>
+                  <p>Follower Reach</p>
+                </div>
+                <div className="engagement">
+                  <h5>56.2k</h5>
+                  <p>Engagement</p>
+                </div>
+              </div>
+              <a href="#">Learn more <span><img src="/images/Vector (2).png" alt="vector-2" /></span></a>
+            </div>
+          </div>
+          <div className="clothing-div">
+            <img className="clothing-img" src="/images/image/image03.png" alt="image03" />
+            <div className="card-div">
+              <h5 className="rebel">Rebel clothing</h5>
+              <div className="followers-div">
+                <div className="followers">
+                  <h5>6.7M</h5>
+                  <p>Follower Reach</p>
+                </div>
+                <div className="engagement">
+                  <h5>56.2k</h5>
+                  <p>Engagement</p>
+                </div>
+              </div>
+              <a href="#">Learn more <span className='span'><img src="/images/Vector (2).png" alt="vector-2" /></span></a>
+            </div>
+          </div>
+          <div className="clothing-div">
+            <img className="clothing-img clothing-img-3" src="/images/image/image02.png" alt="image02" />
+            <div className="card-div">
+              <h5 className="rebel">Rebel clothing</h5>
+              <div className="followers-div">
+                <div className="followers">
+                  <h5>6.7M</h5>
+                  <p>Follower Reach</p>
+                </div>
+                <div className="engagement">
+                  <h5>56.2k</h5>
+                  <p>Engagement</p>
+                </div>
+              </div>
+              <a href="#">Learn more <span><img src="/images/Vector (2).png" alt="vector-2" /></span></a>
+            </div>
+          </div>
+        </div>
+        <div className='section-3 solution-div b'>
+          <h3>Happy Clients</h3>
+          <p>The Marketing Accountability Standards Board (MASB) endorses the definitions, purposes, and constructs of classes</p>
+        </div>
+      <div className="main-testifier">
+      <div className="testifier">
+        <img className='client' src="/images/clients/profile (1).png" alt="profile" />
+        <h5 className="first">Not weekly or monthly like other sites out there. This ensures that we offer prospective homebuyers and investors with the freshest-hottest deals on the internet.</h5>
+        <h5 className="second">Rowhan Smith</h5>
+        <p>CEO, Foreclosure</p>
+      </div>
+      <img className="testifier-img" src="/images/clients/image.png" alt="clients-img" />
+      </div>
+      <div className='subscribe'>
+        <h4>Subscribe our newsletter</h4>
+        <p>Join thousands of marketers and entrepreneurs for a 2-day event at the forefront of social commerce.</p>
+        <button type="button">Request Demo <span>
+              <img src='/images/Vector.png' alt="icon" />
+            </span>
+            </button>
+      </div>
+      </main>
+      <footer>
+        <h5>Besnik.</h5>
+        <div>
+          <ul>
+            <li>Solution</li>
+            <li>Plans</li>
+            <li>Resource</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div className='social-icon'>
+          <img src="/images/social-icons/Vector (4).png" alt='icon' />
+          <img src="/images/social-icons/Group 77.png" alt='icon' />
+          <img src="/images/social-icons/Vector (6).png" alt='icon' />
+        </div>
+      </footer>
     </div>
   );
 }
